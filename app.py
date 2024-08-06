833030c259af26bbc1ac825dc14e4a6fe334c94d
Bhatt Akshat <akshatbhatt0786@gmail.com>
2024-08-06 20:39:16 +0530

    mainPublish

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
